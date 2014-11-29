6f8ac65588208a69803b88e9c53d2929f56018f3
Lean Liam <lean.liam@gmail.com>
2014-11-28 21:45:29 -0800

    added println in buttonPressed function

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBAction func buttonPressed(sender: AnyObject) {
         
+        println("working")
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
