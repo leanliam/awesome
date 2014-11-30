568789aa116fb7ccd0ea682e79dc7d1cbfb77b67
Lean Liam <lean.liam@gmail.com>
2014-11-29 17:56:37 -0800

    Prevented overwriting a button that has already been pressed. In addition to the codes, we set a Tag for each button starting with number 0, matching our array sequence

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -11,22 +11,35 @@ import UIKit
 class ViewController: UIViewController {
     
     var goNumber = 1
+    
+    // 0 = empty, 1 = O, 2 = X
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     @IBOutlet weak var button0: UIButton!
     
     @IBAction func buttonPressed(sender: AnyObject) {
         
+        if gameState[sender.tag]==0 {
+        
         var image = UIImage()
         
         if (goNumber%2==0) {
-            image = UIImage(named: "O.png")!
-        }else{
             image = UIImage(named: "X.png")!
+            
+            gameState[sender.tag] = 2
+            
+        }else{
+            image = UIImage(named: "O.png")!
+            
+            gameState[sender.tag] = 1
         }
         
         goNumber++
         
         sender.setImage(image, forState: .Normal)
+            
+        }
         
     }
     override func viewDidLoad() {
