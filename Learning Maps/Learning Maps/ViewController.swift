7b5d95fd82d9c7de2a3ed59703e6dba8d4ae88e0
Lean Liam <lean.liam@gmail.com>
2014-12-07 10:33:48 -0800

    Imported MapKit and added a class called MKMapViewDelegate

---
+++ Learning Maps/Learning Maps/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Learning Maps
+//
+//  Created by leanliam on 12/7/14.
+//  Copyright (c) 2014 Appfish. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
