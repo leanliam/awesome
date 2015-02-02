659b3408cf65649696bcddfbf4bff8a18752a5fe
Lean Liam <lean.liam@gmail.com>
2015-02-01 20:47:49 -0800

    App - Segue
    An app with a TableView and when you tap any elements within that TableView it switches over to a different view and also provides that new view with the detail of which cell from the table you tapped on.

---
+++ Segue App/Segue App/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Segue App
+//
+//  Created by leanliam on 2/1/15.
+//  Copyright (c) 2015 Appfish. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
